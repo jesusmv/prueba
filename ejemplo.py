6f2c87bba5a380e4ec2ab3893f38ba653e0a5ce9
CHUY <jesus-chivas-77@hotmail.com>
2014-10-29 18:14:38 -0600

    mensaje3

---
+++ ejemplo.py
@@ -0,0 +1,50 @@
+__autor__ = 'chuy'
+
+def sumaDos():
+        print 10 + 20
+def division(a , b):
+    result = a / b
+    print result
+
+def cast():
+    t=10
+    f=10.5
+    
+
+    int(10.5)
+    print
+
+class Estudiante(object):
+    def__init__(self, "nombre",edad):
+        self.nombre=nombre
+        self.edad=edad
+    def hola(self):
+        return self.nombre
+    def esMayor(self):
+        if self,edad>=18:
+            return True
+        else
+            return False
+
+
+    
+def main():
+    sumaDos()
+    division(100.54, 2100.0)
+    e=Estudiante("jesus", 15)
+    print "hola %s" % e.hola()
+    
+    if e.esMayor():
+        print"Es mayor de edad"
+    else
+        print "Es menor de edad"
+    
+    contador = 0
+    while contador<= 10;
+        print contador
+        contador+=1
+
+if __name__=="__main__":
+     main()
+
+
